e2720e3638f7dc4aada019dd607f134d3da3c698
Rahul Eapen George <40990704+rahuleapen@users.noreply.github.com>
2019-10-01 14:12:34 +0530

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,53 @@
+class Account:
+
+def init (self)
+self.accno-int(input("enter the account
+self.name input("enter the accountant
+self.balance float(input("enter the
+def withdraw(self
+n float(input("enter the amount")
+self balance self.balance-n
+def deposit(self)
+
+float(input("enter the amount"))
+self balance self.balance
+def showbalance(self)
+
+print(self.accno, self.name, self.balance)
+
+x=Account:
+y-Account()
+z int(input("select your account: 1.x 2.7">
+pif(z):
+
+c = int(input("enter the option : 1. withdraw 2.deposit 3.show balance >>
+if t):
+x-withdraw()
+
+elif (c= 2):
+x.deposit()
+elif (c=3) :
+
+X.showbalance()
+else:
+
+print("invalid option")
+elif(z=2)
+
+C int(input("enter the option : 1. withdraw 2. deposit 3.She
+if ( 1.)
+
+y.withdraw()
+
+elif ( m2):
+
+y.d
+elif ( )
+
+y.showbalance()
+else:
+
+print("invalid option")
+else:
+
+print("invalid account")
